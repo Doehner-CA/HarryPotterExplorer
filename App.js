868702458bd6c56edcd100d6290c7383d5381bc1
Doehner <doehner9808@gmail.com>
2025-11-09 19:10:34 -0500

    added result section and styled it.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, Text,View, TextInput, Pressable} from 'react-native';
+import { StyleSheet, Text,View, TextInput, Pressable, ActivityIndicator, FlatList} from 'react-native';
 
 export default function App() {
   // category(characters, spells, houses, books)
@@ -85,6 +85,29 @@ export default function App() {
             <Text style={[styles.categoryButtonText, category === 'books' && styles.categoryButtonTextActive]}>Books</Text>
           </Pressable>
         </View>
+      </View>
+
+      {/* RESULTS*/}
+      <View style={styles.resultsSection}>
+        {loading ? (//if loading is true, show the loading indicator, if false, check error state
+          <ActivityIndicator size="large" color="#740001" />
+        ) : error ? (//if error true, show error msg, if not, check if resultsList is empty
+          <Text style={styles.errorText}>{error}</Text>
+        ) : results.length === 0 ? (// if resultsList is empty, show prompt, if not, show flatlist
+          <Text style={styles.placeholderText}>
+            Select a category and search to explore!
+          </Text>
+        ) : (
+          <FlatList
+            data={results}
+            keyExtractor={(item, index) => `${category}-${index}`}
+            renderItem={({ item }) => (
+              <View style={styles.resultItem}>
+                <Text style={styles.resultText}>{JSON.stringify(item)}</Text>
+              </View>
+            )}
+          />
+        )}
       </View>
     </View>
   );
@@ -189,5 +212,36 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: 'bold',
     color: '#D3A625',
-  }
+  },
+
+  // Results
+  resultsSection: {
+    flex: 1,
+    padding: 15,
+  },
+  placeholderText: {
+    textAlign: 'center',
+    color: '#95A5A6',
+    fontSize: 16,
+    marginTop: 50,
+    fontStyle: 'italic',
+  },
+  errorText: {
+    textAlign: 'center',
+    color: '#C0392B',
+    fontSize: 16,
+    marginTop: 20,
+  },
+  resultItem: {
+    backgroundColor: '#FFFFFF',
+    padding: 15,
+    borderRadius: 8,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#E0E0E0',
+  },
+  resultText: {
+    fontSize: 14,
+    color: '#2C3E50',
+  },
 });
